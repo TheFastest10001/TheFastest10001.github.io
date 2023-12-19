8fdbacf668cd02172c2af874fa245b89bac3d255
TheFastest10001 <100816504+TheFastest10001@users.noreply.github.com>
2023-12-19 18:58:05 -0500

    Removed CORS policy stuff

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ const getQuoteBtn = document.querySelector("#getQuoteBtn");
 const displayQuote = document.querySelector("#displayQuote");
 const displayAuthor = document.querySelector("#displayAuthor");
 const quotesApi = "https://api.quotable.io/random";
-// const express = require("express");
-// const cors = require("cors");
-
-// const corsOptions = {
-//   origin: "http://127.0.0.1:5173",
-// };
-
-// const app = express();
-// app.use(cors(corsOptions));
 
 let getQuote = () => {
     fetch(quotesApi).then((data) => data.json()).then((item) => {
